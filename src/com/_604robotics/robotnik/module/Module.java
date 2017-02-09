4e072d15ab708042ec3ce61bb321800a3b584b0a
squeakadoodle <squeakadoodle6084@gmail.com>
2017-02-08 16:33:51 -0800

    Fix boolean error with previous error throwing logic
    (cherry picked from commit fc7e8e62a6ecb7a405d6c95ec9b495f76c1c006a)

--- src/com/_604robotics/robotnik/module/Module.java
+++ src/com/_604robotics/robotnik/module/Module.java
@@ -1,8 +1,16 @@
 package com._604robotics.robotnik.module;
 
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import com._604robotics.robotnik.Settings;
 import com._604robotics.robotnik.action.ActionController;
 import com._604robotics.robotnik.action.controllers.DummyController;
+import com._604robotics.robotnik.data.Data;
 import com._604robotics.robotnik.data.DataMap;
+import com._604robotics.robotnik.exceptions.OverwriteDataError;
+import com._604robotics.robotnik.logging.Logger;
+import com._604robotics.robotnik.trigger.Trigger;
 import com._604robotics.robotnik.trigger.TriggerMap;
 
 /**
@@ -28,22 +36,50 @@ public abstract class Module {
      * @param dataMap The module's data map.
      */
     protected void set (DataMap dataMap) {
-        this.dataMap = dataMap;
+    	int inputLen=0;
+    	Iterator<Entry<String, Data>> count=this.dataMap.iterator();
+    	while ( count.hasNext() ) {
+    		inputLen++;
+    	}
+    	if (inputLen!=0) {
+    		Logger.warn("DataMap has already been set and will be overridden!");
+    		if (Settings.DEBUG_THROW>=Settings.SET_DEBUG) {
+    			throw new OverwriteDataError();
+    		}
+    	}
+    	this.dataMap = dataMap;
     }
 
     /**
      * Sets the trigger map of the module.
-     * @param dataMap The module's trigger map.
+     * @param triggerMap The module's trigger map.
      */
     protected void set (TriggerMap triggerMap) {
+    	int inputLen=0;
+    	Iterator<Entry<String, Trigger>> count=this.triggerMap.iterator();
+    	while ( count.hasNext() ) {
+    		inputLen++;
+    	}
+    	if (inputLen!=0) {
+    		Logger.warn("TriggerMap has already been set and will be overridden!");
+    		if (Settings.DEBUG_THROW>=Settings.SET_DEBUG) {
+    			throw new OverwriteDataError();
+    		}
+    	}
         this.triggerMap = triggerMap;
     }
 
     /**
      * Sets the action controller of the module.
-     * @param dataMap The module's action controller.
+     * @param actionController The module's action controller.
      */
     protected void set (ActionController actionController) {
+    	if (!this.actionController.getClass().isInstance(new DummyController())) {
+    		Logger.warn("ActionController has already been set and will be overridden!");
+    		if (Settings.DEBUG_THROW>=Settings.SET_DEBUG) {
+    			throw new OverwriteDataError();
+    		}
+    	}
         this.actionController = actionController;
     }
 
