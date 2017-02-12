a620c4fb95308f5488a251b79d098fa574b6a124
Steveice10 <Steveice10@gmail.com>
2017-02-12 11:32:01 -0800

    Simplify log management.

--- src/com/_604robotics/robotnik/logging/Logger.java
+++ src/com/_604robotics/robotnik/logging/Logger.java
@@ -15,52 +15,33 @@ public class Logger {
 	private static final SimpleDateFormat LOG_FILE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
 	
 	private static PrintStream LOG_FILE = null;
-	private static FileOutputStream logFileOutput = null;
-	
-	static Thread closeHandles = null;
-	
-	private static boolean isClosed = false;
 	
 	static {
 		try {
-			String fileName = "robotnik_" + LOG_FILE_DATE_FORMAT.format(new Date()) + ".log";
-			File file = new File("/home/lvuser/"+fileName);
-			if(!file.exists() && !file.createNewFile()) {
+			File file = new File("/home/lvuser/robotnik_" + LOG_FILE_DATE_FORMAT.format(new Date()) + ".log");
+			if(file.exists() || file.createNewFile()) {
+				LOG_FILE = new PrintStream(new FileOutputStream(file));
+
+				Runtime.getRuntime().addShutdownHook(new Thread() {
+					@Override
+					public void run() {
+						if (LOG_FILE != null) {
+							warn("Closing log file now; further writes to the log file will fail.");
+							
+							LOG_FILE.flush();
+							LOG_FILE.close();
+							LOG_FILE = null;
+						}
+					}
+				});
+				
+				log("Recording to log file \"" + file.getAbsolutePath() + "\" on roboRIO");
+			} else {
 				warn("Could not create log file.");
 			}
-			
-			logFileOutput = new FileOutputStream(file);
-			LOG_FILE = new PrintStream(logFileOutput);
-			log("Recording to log file \"" + fileName + "\" on cRIO");
-			log("Full path is "+file.getAbsolutePath());
-		} catch(IOException e) {
+		} catch (IOException e) {
 			error("Could not open log file.", e);
 		}
-		closeHandles = new Thread()
-		{
-			public void run()
-			{
-				if (!isClosed)
-				{
-					try {
-						warn("Closing log file now; further writes to the log file will fail");
-						logFileOutput.flush();
-						logFileOutput.close();
-						LOG_FILE.flush();
-						LOG_FILE.close();
-						isClosed=true;
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				else
-				{
-					System.err.println("Logfile is already closed!");
-				}
-			}
-		};
-		Runtime.getRuntime().addShutdownHook(closeHandles);
 	}
 	
 	/**
@@ -86,28 +67,31 @@ public class Logger {
 	 */
 	public static void warn (String message) {
 		record(System.err, "[WARN] " + message);
-		//trace(new Exception());
 	}
 
 	/**
 	 * Logs an error.
 	 * @param message Message to log.
-	 * @param ex Exception to log.
+	 * @param e Exception to log.
 	 */
-	public static void error (String message, Exception ex) {
-		record(System.err, "[ERROR] " + message + ": (" + ex.getClass().getName() + ") " + ex.getMessage());
-		trace(ex);
+	public static void error (String message, Exception e) {
+		record(System.err, "[ERROR] " + message + ": (" + e.getClass().getName() + ") " + e.getMessage());
+		trace(e);
 	}
 
 	private static void record (PrintStream std, String message) {
 		final String line = "(" + LOG_DATE_FORMAT.format(new Date()) + ") " + message;
 		
 		std.println(line);
-		if (LOG_FILE != null) LOG_FILE.println(line);
+		if (LOG_FILE != null) {
+			LOG_FILE.println(line);
+		}
 	}
 
-	private static void trace (Exception ex) {
-		ex.printStackTrace();
-		if (LOG_FILE != null) LOG_FILE.println(ex.toString());
+	private static void trace (Exception e) {
+		e.printStackTrace();
+		if (LOG_FILE != null) {
+			LOG_FILE.println(e.toString());
+		}
 	}
 }
