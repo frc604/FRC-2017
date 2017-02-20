56fcecc9ceb695b26381ece690db7084e3bd1be2
rlee287 <rlee287@yahoo.com>
2017-02-20 11:19:43 -0800

    Remove climber boop sensor
    (cherry picked from commit e6b74019f1d170c9ca705bcd4fa61dabaddc3dc0)

--- src/com/_604robotics/robot2017/modules/Climber.java
+++ src/com/_604robotics/robot2017/modules/Climber.java
@@ -12,13 +12,9 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Victor;
 
 public class Climber extends Module {
-	private final DigitalInput sensor = new DigitalInput(Ports.BUMP_SENSOR);
     private final Victor motor = new Victor(Ports.CLIMBER_MOTOR);
 
     public Climber (){
-    	this.set(new TriggerMap() {{
-            add("Boop", () -> sensor.get());
-        }});
     	
         this.set(new ElasticController() {{
         	addDefault("Idle", new Action() {
