cde21cd3936a28b8ac0e592344aeab0963913947
rlee287 <rlee287@yahoo.com>
2017-03-07 14:57:53 -0800

    Finish implementing word distance algorithm
    This will allow typo suggestions

--- src/com/_604robotics/robotnik/utils/FuzzyMatcher.java
+++ src/com/_604robotics/robotnik/utils/FuzzyMatcher.java
@@ -1,55 +1,67 @@
 package com._604robotics.robotnik.utils;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
-
+/**
+ * Implements fuzzy matching.
+ * This is based on the Levenshtein Distance
+ * http://people.cs.pitt.edu/~kirk/cs1501/Pruhs/Spring2006/assignments/editdistance/Levenshtein%20Distance.htm
+ *
+ */
 public class FuzzyMatcher {
-	private class CacheEntry {
-		public String str1;
-		public String str2;
-		public CacheEntry(String str1, String str2) {
-			this.str1=str1;
-			this.str2=str2;
+	private FuzzyMatcher() {};
+	public static int match(String str1, String str2) {
+		int str1len=str1.length();
+		int str2len=str2.length();
+		int cacheArray[][];
+		cacheArray=new int[str1len+1][str2len+1];
+		cacheArray[0][0]=0;
+		for (int i=1;i<=str1len;i++) {
+			cacheArray[i][0]=i;
 		}
-	}
-
-	private LinkedHashMap<CacheEntry,Integer> MatchLRUCache;
-	private int cacheSize;
-	
-	public FuzzyMatcher (int cacheSize) {
-		this.cacheSize=2*cacheSize;
-	}
-	
-	public int getCacheSize() {
-		return cacheSize/2;
-	}
-
-	public int match(String str1, String str2) {
-		int distance=0;
-		CacheEntry norm = new CacheEntry(str1,str2);
-		CacheEntry rev = new CacheEntry(str2,str1);
-		/* See if in cache */
-		if (MatchLRUCache.containsKey(norm) || MatchLRUCache.containsKey(rev)) {
-			/* Assert same distance for both orderings */
-			if (MatchLRUCache.get(norm) == MatchLRUCache.get(rev)) {
-				return MatchLRUCache.get(norm);
-			} else {
-				/* Mapping is corrupted -> toss out both entries */
-				MatchLRUCache.remove(norm);
-				MatchLRUCache.remove(rev);
-			}
-		} else /* Add to cache and return value */ {
-			if (str1.length()==0) {
-				distance=str2.length();
-				/* Add to cache and return */
-			}
-			if (str2.length()==0) {
-				distance=str2.length();
-				/* Add to cache and return */
+		for (int j=1;j<=str2len;j++) {
+			cacheArray[0][j]=j;
+		}
+		for (int i=1;i<=str1len;i++) {
+			for (int j=1;j<=str2len;j++) {
+				int del1=cacheArray[i-1][j]+1;
+				int del2=cacheArray[i][j-1]+1;
+				int cost=1;
+				if (str1.charAt(i-1)==str2.charAt(j-1)) {
+					cost=0;
+				}
+				int subs=cacheArray[i-1][j-1]+cost;
+				cacheArray[i][j]=Math.min(Math.min(del1, del2), subs);
 			}
 		}
-		return 0;
+//		System.out.print("    ");
+//		for (char ch : str2.toCharArray()) {
+//			System.out.print(ch);
+//			System.out.print(" ");
+//		}
+//		System.out.println();
+//		int index=0;
+//		for (int[] row : cacheArray) {
+//			if (index>=1) {
+//				System.out.print(str1.charAt(index-1));
+//				System.out.print(" ");
+//			} else {
+//				System.out.print("  ");
+//			}
+//			index++;
+//			for (int num : row) {
+//				System.out.print(num);
+//				System.out.print(" ");
+//			}
+//			System.out.println();
+//		}
+		return cacheArray[str1len][str2len];
 	}
+	public static boolean matchThreshold(String str1, String str2, int threshold) {
+		int result=match(str1,str2);
+		return (result<=threshold);
+	}
+//	public static void main(String[] args) {
+//		int print=match("sunday","saturday");
+//		System.out.print("The distance is ");
+//		System.out.println(print);
+//	}
 }
