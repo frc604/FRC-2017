59a510872cf30826ec324ef81bba1afaa0a6a3db
squeakadoodle <squeakadoodle6084@gmail.com>
2017-04-13 10:20:22 -0700

    Added intake

---
+++ src/com/_604robotics/robot2017/modules/Intake.java
@@ -0,0 +1,127 @@
+package com._604robotics.robot2017.modules;
+
+import com._604robotics.robot2017.constants.Calibration;
+import com._604robotics.robotnik.action.Action;
+import com._604robotics.robotnik.action.ActionData;
+import com._604robotics.robotnik.action.controllers.StateController;
+import com._604robotics.robotnik.module.Module;
+import com._604robotics.robotnik.prefabs.devices.MultiOutput;
+import com._604robotics.robotnik.prefabs.trigger.SimpleTriggerMap;
+import com._604robotics.robotnik.trigger.TriggerMap;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Victor;
+
+public class Intake extends Module {
+	// TODO: ports file
+	private Timer timer = new Timer();
+	private Timer timer2 = new Timer();
+	//private boolean gearIn;
+	private boolean init;
+	private boolean running;
+	private MultiOutput mo = new MultiOutput(new PIDOutput[]{
+			new Victor(Calibration.INTAKE_FORWARD_MOTOR), 
+			new Victor(Calibration.INTAKE_REVERSE_MOTOR){{setInverted(true);}}
+		});
+	
+	private DigitalInput boop1 = new DigitalInput(8);
+	private DigitalInput boop2 = new DigitalInput(9);
+	
+	//private SimpleTriggerMap stm;
+	
+	public Intake() {	
+		//stm = new SimpleTriggerMap();
+		//stm.add("Rumble");
+		//gearIn = false;
+		//stm.set("Rumble", false);
+		running = false;
+		this.set(new TriggerMap() {{
+			add("Running", () -> running);
+			add("Boop1", () -> boop1.get());
+			add("Boop2", () -> boop2.get());
+			//add("Rumble", stm.getTrigger("Rumble"));
+		}});
+		this.set(new StateController() {{
+            addDefault("Off", new Action() {
+            	public void begin (ActionData data) {
+            		timer.start();
+            	}
+            	public void run (ActionData data) {
+            		if( !init && timer.get() < 0.5 ) {
+            			running = true;
+            			mo.set(-Calibration.INTAKE_POWER);
+            		}
+            		else {
+            			running = false;
+            			mo.stopMotor();
+            		}
+            	}
+            	@Override
+				public void end (ActionData data) {
+					timer.stop();
+					timer.reset();
+					init = false;
+					running = false;
+					mo.stopMotor();
+				}
+            });
+
+            add("Forward", new Action() {
+                @Override
+                public void run (ActionData data) {
+                	running = true;
+                	//if( !boop1.get() && !boop2.get() ) {
+            		mo.set(-Calibration.INTAKE_POWER);
+            		//stm.set("Rumble", false);
+            		//gearIn = false;
+            		timer2.reset();
+            		timer2.stop();
+                	//}
+                	/*
+                	else {
+                		if( !gearIn ) {
+                			gearIn = true;
+                			timer2.start();
+                		}
+                		else if( timer2.get() < 0.5 ) {
+                			stm.set("Rumble", true);
+                		}
+                		else {
+                			stm.set("Rumble", false);
+                		}
+                	}
+                	*/
+            		/*
+                	else {
+                		mo.stopMotor();
+                	}
+                	*/
+                }
+                @Override
+                public void end (ActionData data) {
+                	running = true;
+                	mo.stopMotor();
+                }
+            });
+            
+            add("Reverse", new Action() {
+                @Override
+                public void run (ActionData data) {
+                	running = true;
+                	mo.set(Calibration.INTAKE_POWER);
+                }
+                @Override
+                public void end (ActionData data) {
+                	running = false;
+                	mo.stopMotor();
+                }
+            });
+        }});
+	}
+	@Override
+	protected void begin() {
+		init = true;
+	}
+}
